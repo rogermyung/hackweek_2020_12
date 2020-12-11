384ebfa11f5a9d4a2e583ae03d85363e55994c90
rogermyung <61071257+rogermyung@users.noreply.github.com>
2020-12-11 11:21:55 -0500

    Update helm_command.sh

--- helm_command.sh
+++ helm_command.sh
@@ -4,4 +4,4 @@ APP_NAME="ssr-example"
 NAMESPACE="ssr"
 YAML_FILE="yaml/helm_output.yml"
 VALUES_FILE="helm_values.yaml"
-helm template $APP_NAME app-template --repo https://git.rockfin.com/pages/helm/ql-helm-charts --namespace $NAMESPACE  --values $HELM_VALUES > $YAML_FILE
+helm template $APP_NAME app-template --repo https://git.rockfin.com/pages/helm/ql-helm-charts --namespace $NAMESPACE  --values $VALUES_FILE > $YAML_FILE
