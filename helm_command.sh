10b3c23ce61ea6769378f7dde3a17fad6d966bdb
Myung, Roger <RogerMyung@quickenloans.com>
2020-12-11 10:50:21 -0500

    fix helm command

--- helm_command.sh
+++ helm_command.sh
@@ -1,5 +1,7 @@
+#this uses relative paths and needs to be run from the directory it lives in.
 rm -f yaml/*
 APP_NAME="ssr-example"
 NAMESPACE="ssr"
 YAML_FILE="yaml/helm_output.yml"
+VALUES_FILE="helm_values.yaml"
 helm template $APP_NAME app-template --repo https://git.rockfin.com/pages/helm/ql-helm-charts --namespace $NAMESPACE  --values $HELM_VALUES > $YAML_FILE
